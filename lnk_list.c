ca2a7ef59aade57d6817cbe91a87018f60029eda
akino <scarletskynned@gmail.com>
2023-12-25 14:26:37 -0300

    added pop function

--- lnk_list.c
+++ lnk_list.c
@@ -53,33 +53,36 @@ t_list      *lst_delete(t_list *head, size_t index);
 	return (head)
 }
 
-t_list      *lst_pop(t_list *head, size_t index)
+t_list      *lst_pop(t_list **head, size_t index)
 {
 	t_list	*curr;
-	t_list	*pop;
 	t_list	*next;
+	t_list	*pop;
 	size_t	i;
 
 	if (!head)
 		return (NULL);
-	curr = head;
+	curr = *head;
 	i = 0;
-	while (curr)
+	if (index == 0)
+		*head = (*head)->next;
+	while (curr->next)
 	{
-		next = curr->next;
-		if (i == index)
+		next = (curr->next)->next;;
+		if ((i + 1) == index)
 		{
-			if (index == 0)
-				head = head->next;
-			*pop = ft_memdup((void *)curr, sizeof(t_list));
-			free(curr);
-			curr = next;
+			pop = ft_memdup(curr->next, sizeof(t_list));
+			curr->next = next;
+			pop->next = NULL;
 			return (pop);
 		}
 		i++;
-		curr = curr->next;
+		curr = curr-next;
 	}
 	return (NULL);
 }
-t_list      *lst_slice(t_list *head, size_t start, size_t end);
+t_list      *lst_slice(t_list *head, size_t start, size_t end)
+{
+	
+}
 t_list      *lst_extend(t_list *head, size_t index);
