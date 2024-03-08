24609e37bfbe3df581930b7418660983644453c8
Charlotte <scarletskynned@gmail.com>
2024-03-08 09:22:53 -0300

    added remove from function

--- lists_1/linked_list_removal_extra.c
+++ lists_1/linked_list_removal_extra.c
@@ -1,5 +1,7 @@
 #include "linked_lists.h"
 
+static void	remove(t_list *node, void (*del)(void *dt));
+
 void	lst_remove_if(t_list **head, int (*cmp)(void *dt), void (*del)(void *dt))
 {
 	t_list	*prev
@@ -31,7 +33,29 @@ void	lst_remove_if(t_list **head, int (*cmp)(void *dt), void (*del)(void *dt))
 
 void	lst_del_from(t_list **head, t_list *from, t_list *to, void (*f)(void *dt))
 {
-	if (!head || !*head || !from)
-		return ;
+	t_list	*safe;
+	t_list	*prev;
+	t_list	*tmp;
 
+	if (!head || !*head || !from || !f)
+		return ;
+	tmp = *head;
+	prev = *head;
+	safe = (*head)->next;
+	while (tmp && tmp != from)
+	{
+		if (prev->next == tmp)
+			tmp = tmp->next;
+		tmp = tmp->next;
+	}
+	while (tmp && tmp != to)
+	{
+		safe = tmp->next;
+		if (*head == tmp)
+			*head = safe;
+		f(tmp->data);
+		free(tmp);
+		tmp = safe;
+	}
+	prev->next = safe;
 }
